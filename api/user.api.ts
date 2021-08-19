4d68bcbc1b293f78bc4c6b8db3195684afc6282c
KirillNikoda <nikodakirill386@gmail.com>
2021-08-20 01:21:32 +0300

    added registering and logging functional

--- api/user.api.ts
+++ api/user.api.ts
@@ -1,19 +1,21 @@
 import axios from 'axios';
 import { baseUrl } from 'constants/api.constants';
-import { RegisterUserDto } from 'models/user.models';
-import { LoginUserDto } from 'store/thunks/userThunks';
+import { LoginUserDto, RegisterUserDto } from 'models/user.models';
 
-export const userApi = {
+export const authApi = {
 	login: async (user: LoginUserDto) => {
 		const { data } = await axios.post(`${baseUrl}/auth/login`, user);
 		return data;
 	},
 	register: async (user: RegisterUserDto) => {
-		const { data } = await axios.post(`${baseUrl}/auth/register`, user);
+		const { data } = await axios.post(`${baseUrl}/auth/register`, {
+			...user,
+			role: 'user'
+		});
 		return data;
 	},
 	getMe: async () => {
-		const { data } = await axios.get(`${baseUrl}/auth/get-me`, {
+		const { data } = await axios.get(`${baseUrl}/auth/me`, {
 			headers: {
 				authorization: localStorage.getItem('token')
 			}
---
+++ pages/auth/index.tsx
@@ -0,0 +1,48 @@
+import AuthCard from 'components/auth/AuthCard';
+import { LoginUserDto, RegisterUserDto } from 'models/user.models';
+import { useState } from 'react';
+import { useAppDispatch } from 'store/hooks';
+import styled from 'styled-components';
+import { AuthWrapper } from './Styles';
+import { register, login } from '../../store/thunks/userThunks';
+const CommonBlock = styled.div`
+	margin: 0 auto;
+	max-width: 1220px;
+	height: 50vh;
+	background-color: #f3f2f2;
+	border-radius: 20px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Auth = () => {
+	const [isSignUp, setIsSignUp] = useState<boolean>(true);
+	const dispatch = useAppDispatch();
+
+	async function signUp(user: any) {
+		dispatch(register(user)).then((res) => {
+			setIsSignUp(!isSignUp);
+		});
+	}
+
+	async function signIn(user: any) {
+		dispatch(login(user)).then((res: any) =>
+			localStorage.setItem('token', res.payload.access_token)
+		);
+	}
+
+	return (
+		<CommonBlock>
+			<AuthWrapper>
+				{isSignUp ? (
+					<AuthCard onClick={signUp} btnText={'Зарегистрироваться'} />
+				) : (
+					<AuthCard btnText={'Войти'} onClick={signIn} />
+				)}
+			</AuthWrapper>
+		</CommonBlock>
+	);
+};
+
+export default Auth;
